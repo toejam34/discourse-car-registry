cd74e62f5d59a32e9ed3d5792386a5222b2a11e7
toejam34 <57115662+toejam34@users.noreply.github.com>
2026-08-28 22:14:43 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,51 +1,42 @@
 # frozen_string_literal: true
 
 # name: discourse-car-registry
-# about: Vehicle and Car Registry directory for Discourse
-# version: 1.0.3
-# authors: TurboRenault / Antigravity
-# url: https://github.com/toejam34/discourse-car-registry
-# required_version: 2.7.0
+# about: A registry for car owners on Discourse
+# version: 0.1
+# authors: Andy
+# url: https://github.com/your-repo/discourse-car-registry
 
 enabled_site_setting :car_registry_enabled
 
-register_asset 'stylesheets/car-registry.scss'
-register_svg_icon 'car' if respond_to?(:register_svg_icon)
+register_asset "stylesheets/car-registry.scss"
 
 after_initialize do
   module ::CarRegistry
-    PLUGIN_NAME = 'discourse-car-registry'
-
-    class Engine < ::Rails::Engine
-      engine_name PLUGIN_NAME
-      isolate_namespace CarRegistry
-    end
+    PLUGIN_NAME = "discourse-car-registry"
   end
 
-  require_relative 'app/models/car_model'
-  require_relative 'app/models/car_location'
-  require_relative 'app/models/car_registry_entry'
-  require_relative 'app/serializers/car_location_serializer'
-  require_relative 'app/serializers/car_model_serializer'
-  require_relative 'app/serializers/car_registry_entry_serializer'
-  require_relative 'app/controllers/car_registry/cars_controller'
-  require_relative 'app/controllers/car_registry/admin/models_controller'
-  require_relative 'app/controllers/car_registry/admin/locations_controller'
+  require_relative "app/models/car_model"
+  require_relative "app/models/car_location"
+  require_relative "app/models/car_registry_entry"
+  require_relative "app/serializers/car_model_serializer"
+  require_relative "app/serializers/car_location_serializer"
+  require_relative "app/serializers/car_registry_entry_serializer"
+  require_relative "app/controllers/car_registry/cars_controller"
+  require_relative "app/controllers/car_registry/admin/models_controller"
+  require_relative "app/controllers/car_registry/admin/locations_controller"
 
-  Discourse::Application.routes.append do
-    get '/cars(.:format)' => 'car_registry/cars#index'
-    get '/cars/meta(.:format)' => 'car_registry/cars#meta'
-    post '/cars(.:format)' => 'car_registry/cars#create'
-    put '/cars/:id(.:format)' => 'car_registry/cars#update'
-    delete '/cars/:id(.:format)' => 'car_registry/cars#destroy'
+  Discourse::Application.routes.draw do
+    get "/cars" => "car_registry/cars#index"
+    get "/cars/meta" => "car_registry/cars#meta"
+    post "/cars" => "car_registry/cars#create"
+    put "/cars/:id" => "car_registry/cars#update"
+    delete "/cars/:id" => "car_registry/cars#destroy"
 
-    scope '/admin/cars', as: 'admin_cars' do
-      resources :models, controller: 'car_registry/admin/models'
-      resources :locations, controller: 'car_registry/admin/locations'
+    get "/cars/*path" => "car_registry/cars#index"
+
+    namespace :admin, constraints: StaffConstraint.new do
+      resources :car_models, only: [:index, :create, :update, :destroy], controller: "car_registry/admin/models"
+      resources :car_locations, only: [:index, :create, :update, :destroy], controller: "car_registry/admin/locations"
     end
-  end
-
-  add_to_serializer(:user_card, :cars_count) do
-    ::CarRegistry::CarRegistryEntry.where(user_id: object.id).count
   end
 end
